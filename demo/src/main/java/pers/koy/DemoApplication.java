dda46cff93ecc757e9e43490dfe851268afd35e6
renky <koy_ren@126.com>
2022-11-19 11:59:47 +0800

    增加sqlite数据源

--- demo/src/main/java/pers/koy/DemoApplication.java
+++ demo/src/main/java/pers/koy/DemoApplication.java
@@ -1,12 +1,14 @@
 package pers.koy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"pers.koy.dao", "pers.koy.dao"})
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
